df6efa45ae390d7ff60cbb7cc2cb2badc6b654d1
Microsoft <Microsoft@Microsoft-PC>
2014-08-16 01:59:59 +0900

    漆黒の堕天使

--- src/arcircle/ftsim/state/inputname/InputNameModel.java
+++ src/arcircle/ftsim/state/inputname/InputNameModel.java
@@ -7,21 +7,39 @@ import arcircle.ftsim.keyinput.KeyListner;
 import arcircle.ftsim.state.InputNameState;
 
 public class InputNameModel implements KeyListner {
-
 	private InputNameState inState;
-
 	public int x = 100;
 	public int y = 100;
 	public String message = "S e l e c t  Y o u r N a m e ";
 	private String charactor = "";
+	/**
+	 * ひらがな=0,  カタカナ=1, 記号=2
+	 */
+	public int charactorOption =0;
 	public int CursorX = 0;  //カーソルが選択している座標Xを格納
 	public int CursorY = 0;  //カーソルが選択している座標Yを格納
-	public char[][] cursorchar = new char [][] {
+	public char[][][] cursorchar = new char [][][] {
+			{
 			{'あ','か','さ','た','な','は','ま','や','ら','わ','ぁ','っ','が','ざ','だ','ば','ぱ','ー'},
 			{'い','き','し','ち','に','ひ','み','　','り','　','ぃ','ゃ','ぎ','じ','ぢ','び','ぴ','～'},
-			{'う','く','す','つ','ぬ','ふ','む','ゆ','る','を','ぅ','ゅ','ぐ','ず','づ','ぶ','ぷ','☆'},
-			{'え','け','せ','て','ね','へ','め','　','れ','　','ぇ','ょ','げ','ぜ','で','べ','ぺ','卍'},
-			{'お','こ','そ','と','の','ほ','も','よ','ろ','ん','ぉ','ゎ','ご','ぞ','ど','ぼ','ぽ','⑦'}
+			{'う','く','す','つ','ぬ','ふ','む','ゆ','る','を','ぅ','ゅ','ぐ','ず','づ','ぶ','ぷ','〇'},
+			{'え','け','せ','て','ね','へ','め','　','れ','　','ぇ','ょ','げ','ぜ','で','べ','ぺ','〇'},
+			{'お','こ','そ','と','の','ほ','も','よ','ろ','ん','ぉ','ゎ','ご','ぞ','ど','ぼ','ぽ','〇'}
+			},
+			{
+			{'ア','カ','サ','タ','ナ','ハ','マ','ヤ','ラ','ワ','ァ','ッ','ガ','ザ','ダ','バ','パ','ー'},
+			{'イ','キ','シ','チ','ニ','ヒ','ミ','　','リ','　','ィ','ャ','ギ','ジ','ヂ','ビ','ピ','～'},
+			{'ウ','ク','ス','ツ','ネ','フ','ム','ユ','ル','ヲ','ゥ','ュ','グ','ズ','ヅ','ブ','プ','〇'},
+			{'エ','け','セ','テ','ネ','ヘ','メ','　','レ','　','ェ','ョ','ゲ','ゼ','デ','ベ','ペ','〇'},
+			{'オ','コ','ソ','ト','ノ','ホ','モ','ヨ','ロ','ン','ォ','ヮ','ゴ','ゾ','ド','ボ','ポ','〇'}
+			},
+			{
+			{'漆','黒','の','堕','天','使','ま','や','ら','わ','ぁ','っ','が','ざ','だ','ば','ぱ','ー'},
+			{'†','虚','空','骸','懺','ひ','み','　','り','　','ぃ','ゃ','ぎ','じ','ぢ','び','ぴ','～'},
+			{'う','く','す','つ','ぬ','ふ','む','ゆ','る','を','ぅ','ゅ','ぐ','ず','づ','ぶ','ぷ','〇'},
+			{'え','け','せ','て','ね','へ','め','　','れ','　','ぇ','ょ','げ','ぜ','で','べ','ぺ','〇'},
+			{'お','こ','そ','と','の','ほ','も','よ','ろ','ん','ぉ','ゎ','ご','ぞ','ど','ぼ','ぽ','〇'}
+			}
 	};
 
 	public InputNameModel(InputNameState inputNameState) {
@@ -36,8 +54,17 @@ public class InputNameModel implements KeyListner {
 			if(CursorX == 17 && CursorY == 4 ){
 				inState.nextState();							//決定キーが押されたので確定
 			}
-			if(CursorX == 17 && CursorY == 2 ){
+			else if(CursorX == 17 && CursorY == 2 ){
 				charactor = "";										//クリアキーが押されたので名前クリア
+			}
+			else if(CursorX == 17 && CursorY == 3 &&  charactorOption == 0){
+				charactorOption = 1;							//カナ　が押されたのでカナ入力モード
+			}
+			else if(CursorX == 17 && CursorY == 3 &&  charactorOption == 1){
+				charactorOption = 2;							//記号　が押されたので記号入力モード
+			}
+			else if(CursorX == 17 && CursorY == 3 &&  charactorOption == 2){
+				charactorOption = 0;							//ひら　が押されたのでひらがな入力モード
 			}
 			else{
 				addGetCharFromCursor();
@@ -58,11 +85,11 @@ public class InputNameModel implements KeyListner {
 	}
 
 	/**
-	 * カーソルの位置CursorY,CursorXから文字に変換し、String charactorに6文字まで累加算する
+	 * カーソルの位置charactorOption,CursorY,CursorXから文字に変換し、String charactorに6文字まで累加算する
 	 */
 	public void addGetCharFromCursor(){
 		if(charactor.length()>=6){return;}				//名前は6文字まで
-		charactor = charactor + cursorchar[CursorY][CursorX] ;
+		charactor = charactor + cursorchar[charactorOption][CursorY][CursorX] ;
 		return;
 	}
 
