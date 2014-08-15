bc2506aa208ef71f0c2050ed385d08b477fcb4e5
俊樹 <@TOSHIKI>
2014-08-15 23:14:15 +0900

    Imageのnewをコンストラクタへ

--- src/arcircle/ftsim/state/selectgender/SelectGenderView.java
+++ src/arcircle/ftsim/state/selectgender/SelectGenderView.java
@@ -11,13 +11,27 @@ import arcircle.ftsim.main.FTSimulationGame;
 import arcircle.ftsim.renderer.Renderer;
 import arcircle.ftsim.state.SelectGenderState;
 
-public class SelectGenderView implements Renderer{
+public class SelectGenderView implements Renderer {
 	SelectGenderModel sgModel;
 	private SelectGenderState sgState;
+	private Image imageMale;
+	private Image imageFemale;
+	private Image flame;
+
 	public SelectGenderView(SelectGenderModel sgModel, SelectGenderState sgState) {
 		super();
 		this.sgModel = sgModel;
 		this.sgState = sgState;
+
+		try {
+
+			imageMale = new Image("./image/genderimage/male.png");
+			imageFemale = new Image("./image/genderimage/female.png");
+			flame = new Image("./image/genderimage/flame.png");
+
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -28,43 +42,33 @@ public class SelectGenderView implements Renderer{
 		g.setColor(Color.black);
 		g.setFont(sgState.getFont());
 		int messageWidth = sgState.getFont().getWidth(sgModel.message);
-		g.drawString(sgModel.message, (FTSimulationGame.WIDTH - messageWidth) / 2,
-				100);
+		g.drawString(sgModel.message,
+				(FTSimulationGame.WIDTH - messageWidth) / 2, 100);
 
-		Image imageMale;
-		Image imageFemale;
-		Image flame;
-		try {
-			imageMale = new Image("./image/genderimage/male.png");
-			imageFemale = new Image("./image/genderimage/female.png");
-			flame = new Image("./image/genderimage/flame.png");
+		int imageGenderWidth = imageMale.getWidth();
+		int imageGenderHeight = imageMale.getHeight();
+		int flameWidth = flame.getWidth();
+		int flameHeight = flame.getHeight();
 
-			int imageGenderWidth = imageMale.getWidth();
-			int imageGenderHeight = imageMale.getHeight();
-			int flameWidth = flame.getWidth();
-			int flameHeight = flame.getHeight();
-
-
-			if(sgModel.gender == SelectGenderModel.MALE){
-				g.drawImage(imageMale, FTSimulationGame.WIDTH/4-imageGenderWidth/2,
-						FTSimulationGame.HEIGHT/3*2-imageGenderHeight/2);
-				g.drawImage(imageFemale, FTSimulationGame.WIDTH/4*3-imageGenderWidth/2,
-						FTSimulationGame.HEIGHT/3*2-imageGenderHeight/2);
-				g.drawImage(flame, FTSimulationGame.WIDTH/4-flameWidth/2,
-						FTSimulationGame.HEIGHT/3*2-flameHeight/2);
-			}
-			else if(sgModel.gender == SelectGenderModel.FEMALE){
-				g.drawImage(imageMale, FTSimulationGame.WIDTH/4-imageGenderWidth/2,
-						FTSimulationGame.HEIGHT/3*2-imageGenderHeight/2);
-				g.drawImage(imageFemale, FTSimulationGame.WIDTH/4*3-imageGenderWidth/2,
-						FTSimulationGame.HEIGHT/3*2-imageGenderHeight/2);
-				g.drawImage(flame, FTSimulationGame.WIDTH/4*3-flameWidth/2,
-						FTSimulationGame.HEIGHT/3*2-flameHeight/2);
-			}
-
+		if (sgModel.gender == SelectGenderModel.MALE) {
+			g.drawImage(imageMale, FTSimulationGame.WIDTH / 4
+					- imageGenderWidth / 2, FTSimulationGame.HEIGHT / 3 * 2
+					- imageGenderHeight / 2);
+			g.drawImage(imageFemale, FTSimulationGame.WIDTH / 4 * 3
+					- imageGenderWidth / 2, FTSimulationGame.HEIGHT / 3 * 2
+					- imageGenderHeight / 2);
+			g.drawImage(flame, FTSimulationGame.WIDTH / 4 - flameWidth / 2,
+					FTSimulationGame.HEIGHT / 3 * 2 - flameHeight / 2);
+		} else if (sgModel.gender == SelectGenderModel.FEMALE) {
+			g.drawImage(imageMale, FTSimulationGame.WIDTH / 4
+					- imageGenderWidth / 2, FTSimulationGame.HEIGHT / 3 * 2
+					- imageGenderHeight / 2);
+			g.drawImage(imageFemale, FTSimulationGame.WIDTH / 4 * 3
+					- imageGenderWidth / 2, FTSimulationGame.HEIGHT / 3 * 2
+					- imageGenderHeight / 2);
+			g.drawImage(flame, FTSimulationGame.WIDTH / 4 * 3 - flameWidth / 2,
+					FTSimulationGame.HEIGHT / 3 * 2 - flameHeight / 2);
 		}
-		catch (SlickException e) {
-			e.printStackTrace();
-		}
+
 	}
 }
