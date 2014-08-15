afbccfd4711d58ab9f9573e5167f69bcb3c91499
Microsoft <Microsoft@Microsoft-PC>
2014-08-15 12:46:24 +0900

    名前入力画面遷移できたよ一応

--- src/arcircle/ftsim/main/FTSimulationGame.java
+++ src/arcircle/ftsim/main/FTSimulationGame.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import arcircle.JapaneseFont.JapaneseFontGenerater;
 import arcircle.ftsim.state.GameStartState;
+import arcircle.ftsim.state.InputNameState;
 import arcircle.ftsim.state.SelectGenderState;
 import arcircle.ftsim.state.StateConst;
 
@@ -22,12 +23,14 @@ public class FTSimulationGame extends StateBasedGame {
 		super(name);
 		this.addState(new GameStartState(StateConst.GAME_START));
 		this.addState(new SelectGenderState(StateConst.SELECT_GENDER));
+		this.addState(new InputNameState(StateConst.INPUT_NAME));
 	}
 
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
 		this.getState(StateConst.GAME_START).init(container, this);
 		this.getState(StateConst.SELECT_GENDER).init(container, this);
+		this.getState(StateConst.INPUT_NAME).init(container, this);
 		//this.enterState(StateConst.GAME_START);
 
 		//コンストラクタで実行するとエラー発生するよー
--- src/arcircle/ftsim/state/StateConst.java
+++ src/arcircle/ftsim/state/StateConst.java
@@ -3,5 +3,5 @@ package arcircle.ftsim.state;
 public class StateConst {
 	public static final int GAME_START = 0;
     public static final int SELECT_GENDER = 1;
-    public static final int NAME_INPUT = 2;
+    public static final int INPUT_NAME = 2;
 }
