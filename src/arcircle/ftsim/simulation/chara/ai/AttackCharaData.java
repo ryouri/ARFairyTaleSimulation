2bd40d5177c2084f18ad7879a2e104b9c665bfce
misawa <misawa@misawa-tlab-PC>
2014-10-01 13:21:26 +0900

    とりあえず戦闘可能に！ ただし，攻撃範囲内に攻撃対象がいないと動かない アニメーションは全然ついてない

---
+++ src/arcircle/ftsim/simulation/chara/ai/AttackCharaData.java
@@ -0,0 +1,11 @@
+package arcircle.ftsim.simulation.chara.ai;
+
+import java.awt.Point;
+
+import arcircle.ftsim.simulation.chara.Chara;
+
+public class AttackCharaData {
+	public Point attackPoint;
+	public Chara damageChara;
+	public Chara attackChara;
+}
