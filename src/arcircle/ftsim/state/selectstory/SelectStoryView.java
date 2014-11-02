005d0f5d3d435725462cc895a3019bcac151c865
ゆきねこ <@H_MobilePC>
2014-11-02 17:50:11 +0900

    StorySelect_画像の差し替え yukineco

--- src/arcircle/ftsim/state/selectstory/SelectStoryView.java
+++ src/arcircle/ftsim/state/selectstory/SelectStoryView.java
@@ -1,13 +1,11 @@
 package arcircle.ftsim.state.selectstory;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-import arcircle.ftsim.main.FTSimulationGame;
 import arcircle.ftsim.renderer.Renderer;
 import arcircle.ftsim.state.SelectStoryState;
 
@@ -16,6 +14,7 @@ public class SelectStoryView implements Renderer {
 	private SelectStoryState ssState;
 	private Image[] storyTitleImg = new Image[6];
 	private int[][] storyTitlePos = new int[6][2];
+	private Image backGroundImg;
 	private Image flame;
 	private static final String imagePath = "./image";
 
@@ -29,29 +28,30 @@ public class SelectStoryView implements Renderer {
 				storyTitleImg[i] = new Image(imagePath + "/storyTitle0" + (i+1) + ".png");
 			}
 			flame = new Image("./image/frame.png");
+			backGroundImg = new Image("./image/StorySelect.png");
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
 
 		for(int i = 0 ; i < 6 ; i++){
 			storyTitlePos[i][0] = 60 + (i % 3) * 350;
-			storyTitlePos[i][1] = 100 + (i / 3) * 270;
+			storyTitlePos[i][1] = 120 + (i / 3) * 270;
 		}
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) {
-		g.setColor(Color.yellow);
-		g.fillRect(0, 0, FTSimulationGame.WIDTH, FTSimulationGame.HEIGHT);
-
-		g.setColor(Color.black);
-		g.setFont(ssState.getFont());
-		int messageWidth = ssState.getFont().getWidth(ssModel.message);
-		g.drawString(ssModel.message, (FTSimulationGame.WIDTH - messageWidth) / 2, 30);
+//		g.setColor(Color.yellow);
+//		g.fillRect(0, 0, FTSimulationGame.WIDTH, FTSimulationGame.HEIGHT);
+		g.drawImage(backGroundImg,0,0);
+//		g.setColor(Color.black);
+//		g.setFont(ssState.getFont());
+//		int messageWidth = ssState.getFont().getWidth(ssModel.message);
+//		g.drawString(ssModel.message, (FTSimulationGame.WIDTH - messageWidth) / 2, 30);
 
 		int selectStory = ssModel.story;
 		int flamePosX = 50 + (selectStory % 3) * 350;
-		int flamePosY = 90 + (selectStory / 3) * 270;
+		int flamePosY = 110 + (selectStory / 3) * 270;
 
 		g.drawImage(flame, flamePosX, flamePosY);
 
