a3036c428ede4d7fb1c385cf76a1eb14fef077fa
ryosuke <ryosuke@ryosuke-Surface>
2015-01-24 15:35:24 +0900

    新しいルート探索用パッケージの作成

---
+++ src/arcircle/ftsim/simulation/algorithm/route/Astar.java
@@ -0,0 +1,16 @@
+package arcircle.ftsim.simulation.algorithm.route;
+
+import java.util.ArrayList;
+
+
+public class Astar {
+    // オープンリスト
+    private ArrayList openList;
+    // クローズドリスト
+    private ArrayList closedList;
+
+
+    public Astar(){
+
+    }
+}
