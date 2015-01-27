95370b63fea747770941eaf4932382a9fe6038ed
miyazaki <misawa@misawa-MacBook>
2015-01-27 17:33:16 +0900

    敵が最寄りの味方を攻撃するときに、攻撃対象を選択するための抽象クラスを作った

---
+++ src/arcircle/ftsim/simulation/chara/ai/SelectAttackTarget.java
@@ -0,0 +1,24 @@
+package arcircle.ftsim.simulation.chara.ai;
+
+import java.util.ArrayList;
+
+public abstract class SelectAttackTarget {
+
+	// 一番いいアタックキャラデータを頼む
+	public AttackCharaData getAttackTargetCharaData(ArrayList<AttackCharaData> attackCharaArray){
+		int minCost = 9999;
+		AttackCharaData minAttackCharaData = null;
+		for (AttackCharaData acd : attackCharaArray){
+			int cost = calculateCost(acd);
+			if (cost < minCost){
+				minCost = cost;
+				minAttackCharaData = acd;
+			}
+		}
+		return minAttackCharaData;
+	}
+
+	// ヒューリスティック関数は抽象化
+	abstract public int calculateCost(AttackCharaData attackCharaData);
+
+}
