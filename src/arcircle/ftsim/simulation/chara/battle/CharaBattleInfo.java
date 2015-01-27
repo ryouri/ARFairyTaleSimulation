623598c49851d34949fe62c18bc0d952240c08e6
misawa <misawa@misawa-MacBook>
2015-01-27 19:58:27 +0900

    アクセッサの追加

--- src/arcircle/ftsim/simulation/chara/battle/CharaBattleInfo.java
+++ src/arcircle/ftsim/simulation/chara/battle/CharaBattleInfo.java
@@ -1,12 +1,63 @@
 package arcircle.ftsim.simulation.chara.battle;
 
 public class CharaBattleInfo {
+	public CharaBattleInfo(int hp, int damage, boolean twiceAttack, int hitProbability, int deadProbability){
+		this.hp = hp;
+		this.damage = damage;
+		this.twiceAttack = twiceAttack;
+		this.hitProbability = hitProbability;
+		this.deadProbability = deadProbability;
+	}
+
 	private int hp;
 
-	private int power;
+	private int damage;
 	private boolean twiceAttack;
 
 	private int hitProbability;
 
 	private int deadProbability;
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getPower() {
+		return damage;
+	}
+
+	public void setPower(int power) {
+		this.damage = power;
+	}
+
+	public boolean isTwiceAttack() {
+		return twiceAttack;
+	}
+
+	public void setTwiceAttack(boolean twiceAttack) {
+		this.twiceAttack = twiceAttack;
+	}
+
+	public int getHitProbability() {
+		return hitProbability;
+	}
+
+	public void setHitProbability(int hitProbability) {
+		this.hitProbability = hitProbability;
+	}
+
+	public int getDeadProbability() {
+		return deadProbability;
+	}
+
+	public void setDeadProbability(int deadProbability) {
+		this.deadProbability = deadProbability;
+	}
+
+
+
 }
