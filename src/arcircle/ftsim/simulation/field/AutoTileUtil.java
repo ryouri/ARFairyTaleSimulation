52faee2c103a22990797ad1ae1dd425e94c948df
ryouri <misawa330330@gmail.com>
2015-09-13 15:13:53 +0900

    オートタイルの描画プログラムの基本構造を確定

---
+++ src/arcircle/ftsim/simulation/field/AutoTileUtil.java
@@ -0,0 +1,21 @@
+package arcircle.ftsim.simulation.field;
+
+public class AutoTileUtil {
+
+	/**
+	 * 3 * 3のタイルの配置情報を，2 * 2のポイントのオートタイル描画情報を返す
+	 * @param aroudTileArray 3 * 3のタイルの配置情報が格納されている配列
+	 * @return 2 * 2 のPointの配列，オートタイルの描画情報が格納されている
+	 */
+	public static int[][][] aroudTileArrayToDrawTileArray(boolean[][] aroudTileArray) {
+
+
+		return null;
+
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+}
