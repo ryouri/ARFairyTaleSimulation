0c37c4f1c5f34e2c36fa8ddaf8291dac9f6c4692
yukineco <h.imoto.sd.ics.1640@gmail.com>
2015-09-30 19:14:41 +0900

    EffectManagerを作成してみた(動作確認まだ) yukineko

--- src/arcircle/ftsim/simulation/model/effect/EfConst.java
+++ src/arcircle/ftsim/simulation/model/effect/EfConst.java
@@ -3,34 +3,36 @@ package arcircle.ftsim.simulation.model.effect;
 /** effectフォルダ内のファイル名とちゃんと対応づけること
  * @author ゆきねこ */
 public class EfConst {
+	/** チップサイズ*/
+	public final static int CHIP_SIZE = 32;
 	/** 爆発 */
-	public final String BOMB = "bomb";
+	public final static String BOMB = "bomb";
 	/** 闇の炎 */
-	public final String DARKFIRE = "darkfire";
+	public final static String DARKFIRE = "darkfire";
 	/** 暗黒物質 */
-	public final String DARKMATTER = "darkmatter";
+	public final static String DARKMATTER = "darkmatter";
 	/** 炎 */
-	public final String FIRE = "fire";
+	public final static String FIRE = "fire";
 	/** 大地から突き出る槍 */
-	public final String GRAIVE = "graive";
+	public final static String GRAIVE = "graive";
 	/** 回復 */
-	public final String HOLY = "holy";
+	public final static String HOLY = "holy";
 	/** 氷 */
-	public final String ICE = "ice";
+	public final static String ICE = "ice";
 	/** 氷爆弾 */
-	public final String ICEBOMB = "icebomb";
+	public final static String ICEBOMB = "icebomb";
 	/** 閃光弾 */
-	public final String LIGHTNING = "lightning";
+	public final static String LIGHTNING = "lightning";
 	/** 光爆発 */
-	public final String PHOTON = "photon";
+	public final static String PHOTON = "photon";
 	/** パワーダウン */
-	public final String POWERDOWN = "powerdown";
+	public final static String POWERDOWN = "powerdown";
 	/** パワーアップ */
-	public final String POWERUP = "powerup";
+	public final static String POWERUP = "powerup";
 	/** 斬撃 */
-	public final String SLASH = "slash";
+	public final static String SLASH = "slash";
 	/** 音速の衝撃波 */
-	public final String SONICWAVE = "sonicwave";
+	public final static String SONICWAVE = "sonicwave";
 	/** 風 */
-	public final String WIND = "wind";
+	public final static String WIND = "wind";
 }
--- src/arcircle/ftsim/simulation/model/effect/EffectInfo.java
+++
@@ -1,9 +0,0 @@
-package arcircle.ftsim.simulation.model.effect;
-
-
-/** タスクからEffectManagerを呼び出すときに渡すパッケージ
- * @author ゆきねこ */
-public class EffectInfo {
-	public int px,py;
-	public String name;
-}
