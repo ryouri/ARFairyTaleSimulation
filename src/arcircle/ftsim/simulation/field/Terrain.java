aaa1ce3de0e374f9ef54b9c5529165243d31d045
misawa <misawa@misawa-tlab-PC>
2015-10-14 20:42:17 +0900

    [add]クラスと地形のコスト読み込みを追加

--- src/arcircle/ftsim/simulation/field/Terrain.java
+++ src/arcircle/ftsim/simulation/field/Terrain.java
@@ -1,9 +1,15 @@
 package arcircle.ftsim.simulation.field;
 
+import java.util.HashMap;
+
 public class Terrain {
 	public String terrainName;
 	public int mapChipStartX;
 	public int mapChipStartY;
 	public int avoidPoint;
 	public int defencePoint;
+	public HashMap<String, Integer> classNameCostMap;
+	public Terrain() {
+		classNameCostMap = new HashMap<String, Integer>();
+	}
 }
