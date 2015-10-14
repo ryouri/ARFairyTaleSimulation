4b9f1e4cbe96387a2938de96a8dd6a368bbaf99a
ryouri <misawa330330@gmail.com>
2015-10-14 21:06:37 +0900

    [add] 次の状態への移動に開始/ロードの選択状態を考慮(ロードは未実装)

--- src/arcircle/ftsim/state/GameStartState.java
+++ src/arcircle/ftsim/state/GameStartState.java
@@ -19,7 +19,7 @@ public class GameStartState extends KeyInputState {
 
 	public GameStartState(int state) {
 		super(state);
-		
+
 	}
 
 	@Override
@@ -41,11 +41,17 @@ public class GameStartState extends KeyInputState {
 	}
 
 	public void nextState() {
-		SelectGenderState selectGenderState = (SelectGenderState)stateGame.getState(StateConst.SELECT_GENDER);
-		selectGenderState.setLastBGM(bgm);
-		stateGame.enterState(StateConst.SELECT_GENDER,
-				new FadeOutTransition(Color.black, 100),
-				new FadeInTransition(Color.black, 100));
+		// 開始を選択
+		if (gsModel.state == 0){
+			SelectGenderState selectGenderState = (SelectGenderState)stateGame.getState(StateConst.SELECT_GENDER);
+			selectGenderState.setLastBGM(bgm);
+			stateGame.enterState(StateConst.SELECT_GENDER,
+					new FadeOutTransition(Color.black, 100),
+					new FadeInTransition(Color.black, 100));
+		}
+		// ロードを選択
+		if (gsModel.state == 0){
+		}
 	}
 
 	@Override
