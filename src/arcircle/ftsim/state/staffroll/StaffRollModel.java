c6422a93e0194390c292d83ddb1ef6679b0004d6
ryouri <misawa330330@gmail.com>
2015-10-23 19:15:18 +0900

    [add] スタッフロールの描画

--- src/arcircle/ftsim/state/staffroll/StaffRollModel.java
+++ src/arcircle/ftsim/state/staffroll/StaffRollModel.java
@@ -1,11 +1,18 @@
 package arcircle.ftsim.state.staffroll;
 
+import java.io.File;
+
+import org.newdawn.slick.Input;
+
 import arcircle.ftsim.keyinput.KeyInput;
 import arcircle.ftsim.keyinput.KeyListner;
+import arcircle.ftsim.main.FTSimulationGame;
 import arcircle.ftsim.state.StaffRollState;
 
 public class StaffRollModel implements KeyListner{
 	private StaffRollState srState;
+	private File text_file = new File("./Stories/StaffRoll.txt");
+	public int output_height = FTSimulationGame.HEIGHT + 30;
 
 	public StaffRollModel(StaffRollState srState){
 		super();
@@ -14,8 +21,12 @@ public class StaffRollModel implements KeyListner{
 
 	@Override
 	public void keyInput(KeyInput keyInput) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		if (keyInput.isKeyDown(Input.KEY_Z)) {
+			srState.nextState();
+		}
 	}
 
+	public File getTextFile() {
+		return text_file;
+	}
 }
--- src/arcircle/ftsim/state/staffroll/StaffRollView.java
+++ src/arcircle/ftsim/state/staffroll/StaffRollView.java
@@ -1,5 +1,10 @@
 package arcircle.ftsim.state.staffroll;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -23,6 +28,33 @@ public class StaffRollView implements Renderer {
 		g.setColor(Color.gray);
 		g.fillRect(0, 0, FTSimulationGame.WIDTH, FTSimulationGame.HEIGHT);
 
+		g.setColor(Color.white);
+		g.setFont(srState.getFont());
+
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(srModel.getTextFile()));
+		} catch (FileNotFoundException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+		String message;
+		int h = srModel.output_height;
+		try {
+			while((message = br.readLine()) != null) {
+				int messageWidth = srState.getFont().getWidth(message);
+				g.drawString(message,
+						(FTSimulationGame.WIDTH - messageWidth) / 2, h);
+				h += 30;
+			}
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		if (h > FTSimulationGame.HEIGHT /2) {
+			srModel.output_height -= 1;
+		}
 	}
 
 }
