5620ff516e15ed779582ce5a10c573a2462a1d7d
misawa <misawa@misawa-tlab-PC>
2015-10-27 15:53:56 +0900

    [add] 各イベントを確認するクラスを追加

--- src/arcircle/ftsim/simulation/model/task/ConfirmTargetTask.java
+++ src/arcircle/ftsim/simulation/model/task/ConfirmTargetTask.java
@@ -2,23 +2,59 @@ package arcircle.ftsim.simulation.model.task;
 
 import org.newdawn.slick.Graphics;
 
+import arcircle.ftsim.simulation.event.Event;
 import arcircle.ftsim.simulation.event.EventManager;
+import arcircle.ftsim.simulation.model.task.confirmtarget.ConfirmTargetTaskLogic;
 
 public class ConfirmTargetTask extends Task {
-	EventManager eventManager;
+	private EventManager eventManager;
+	private ConfirmTargetTaskLogic confirmTargetTaskLogic;
+
+	public EventManager getEventManager() {
+		return eventManager;
+	}
 
 	public ConfirmTargetTask(TaskManager taskManager, EventManager eventManager) {
 		super(taskManager);
 		this.eventManager = eventManager;
+
+		Event event = eventManager.getWinConditionEachPhaseArray().get(0).get(0);
+
+
+		String className = ConfirmTargetTaskLogic.packageName
+				+ ConfirmTargetTaskLogic.classList[event.eventType];
+
+		Class<?> recommBuilderClass = null;
+		try {
+			recommBuilderClass = Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		if (recommBuilderClass == null) {
+			System.err.println("Generate ConfirmTargetTask Error");
+			System.exit(1);
+		}
+
+		try {
+			this.confirmTargetTaskLogic = (ConfirmTargetTaskLogic) recommBuilderClass.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		confirmTargetTaskLogic.setConfirmTargetTask(this);
 	}
 
 	@Override
 	public void render(Graphics g, int offsetX, int offsetY, int firstTileX,
 			int lastTileX, int firstTileY, int lastTileY) {
+		confirmTargetTaskLogic.render(g, offsetX, offsetY, firstTileX, lastTileX, firstTileY, lastTileY);
 	}
 
 	@Override
 	public void update(int delta) {
+		confirmTargetTaskLogic.update(delta);
 		taskManager.taskEnd();
 	}
 }
---
+++ src/arcircle/ftsim/simulation/model/task/confirmtarget/ConfirmCharaDie.java
@@ -0,0 +1,14 @@
+package arcircle.ftsim.simulation.model.task.confirmtarget;
+
+import org.newdawn.slick.Graphics;
+
+public class ConfirmCharaDie extends ConfirmTargetTaskLogic {
+	@Override
+	public void render(Graphics g, int offsetX, int offsetY, int firstTileX,
+			int lastTileX, int firstTileY, int lastTileY) {
+	}
+
+	@Override
+	public void update(int delta) {
+	}
+}
